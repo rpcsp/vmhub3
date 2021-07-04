b08756958bf722671a94c98bd06d66961aeac075
rpcsp <rpcsp@rpcsp>
2021-07-04 10:36:56 +0100

    Adding requirements to setup

--- setup.py
+++ setup.py
@@ -9,20 +9,35 @@ setuptools.setup(
     author="rpcsp",
     author_email="pcunha@hotmail.com",
     description="python module to send instructions to VM Hub 3 routers",
+    license="https://github.com/rpcsp/vmhub3/blob/main/LICENSE",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/rpcsp/vmhub3",
     project_urls={
         "Project page": "https://github.com/rpcsp/vmhub3",
     },
     classifiers=[
-        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
+    install_requires=[
+        "requests",
+        "xmltodict",
+    ],
+    test_requires=[
+        "pytest",
+        "pytest-cov",
+        "pytest-flake8",
+    ],
     entry_points="""\
     [console_scripts]
     vmhub3 = vmhub3.vmhub3:main
